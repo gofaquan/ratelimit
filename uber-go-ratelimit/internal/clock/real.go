087fe938b5f22d5c3494f01fef86c0f3474e541b
gofaquan <2660796265@qq.com>
2022-02-16 19:03:27 +0800

    uber-go/ratelimit real.go.go translated

--- uber-go-ratelimit/internal/clock/real.go
+++ uber-go-ratelimit/internal/clock/real.go
@@ -1,42 +1,27 @@
-// Copyright (c) 2016 Uber Technologies, Inc.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
 package clock
 
 import "time"
 
-// clock implements a real-time clock by simply wrapping the time package functions.
+// clock 通过简单地包装 时间包函数(time) 来实现实时时钟。
 type clock struct{}
 
-// New returns an instance of a real-time clock.
+// New 返回一个实时时钟的实例。
 func New() Clock {
 	return &clock{}
 }
 
+// After 返回过去 d 后的时间
 func (c *clock) After(d time.Duration) <-chan time.Time { return time.After(d) }
 
+// AfterFunc 使用 time 包中的 AfterFunc
 func (c *clock) AfterFunc(d time.Duration, f func()) {
-	// TODO maybe return timer interface
+	// TODO 可能会返回 timer 的接口
 	time.AfterFunc(d, f)
+	//AfterFunc等待持续时间结束，然后在自己的 goroutine 中调用 f 。它返回一个 Timer，可以使用它的 Stop 方法取消调用。
 }
 
+// Now 返回现在的时间
 func (c *clock) Now() time.Time { return time.Now() }
 
+// Sleep 睡眠 d 的时间
 func (c *clock) Sleep(d time.Duration) { time.Sleep(d) }

04e5ef8e5ee9f5c1d09ebf014ca814a30ea7af5a
gofaquan <2660796265@qq.com>
2022-02-16 19:03:08 +0800

    uber-go/ratelimit interface.go.go translated

--- uber-go-ratelimit/internal/clock/interface.go
+++ uber-go-ratelimit/internal/clock/interface.go
@@ -1,32 +1,11 @@
-// Copyright (c) 2016 Uber Technologies, Inc.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
 package clock
 
 import "time"
 
-// Clock represents an interface to the functions in the standard library time
-// package. Two implementations are available in the clock package. The first
-// is a real-time clock which simply wraps the time package's functions. The
-// second is a mock clock which will only make forward progress when
-// programmatically adjusted.
+// Clock 表示函数的标准库时间的接口
+//package clock(本包) 中有以下两种实现:
+//第一个是一个实时时钟，它只是封装了时间包的函数。
+//第二个是一个模拟时钟，只会在以编程方式调整。
 type Clock interface {
 	AfterFunc(d time.Duration, f func())
 	Now() time.Time
